a15079255aea28f1d554a4d2205aadac0e75f423
LucasNemeth <l_orion_n@hotmail.com>
2020-02-09 16:30:22 -0500

    rebuilt the JS

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
 $(function() {
-    let index= Math.floor(Math.random() * 20);
-
-        d=new Date();
-    
-    // let index=0;
-    let randNum=Math.random();
-    let randomImg = "https://www.thispersondoesnotexist.com/image?";
-    $('.displayImg').attr("src", randomImg + d.getTime());
-    // $('.secondImg').attr("src", randomImg + d.getTime())
-
-
-
-    let realImg = [
-        "/assets/reals/real2.jpg",
-        "/assets/reals/placeholder1.jpg",
-        "/assets/reals/real1.jpg",
-        "/assets/reals/real3.jpeg.jpg",
-        "/assets/reals/placeholder5.jpg"
+    //gathering all the materials. 
+    const realImg = [
+        "./assets/reals/placeholder1.jpg",
+        "./assets/reals/placeholder2.jpg",
+        "./assets/reals/placeholder3.jpg",
+        "./assets/reals/real1.jpg",
+        "./assets/reals/placeholder4.jpg",
+        "./assets/reals/placeholder5.jpg",
+        "./assets/reals/real2.jpg",
+        "./assets/reals/real3.jpg"
     ]
+    const randomFakeImg = "https://www.thispersondoesnotexist.com/image?";
 
-    
+    d = new Date();
 
-    function changeImage() {
-        // const img = $('content1','content2').$('img')[0]
-        // img.src = firstImg[index] , secondImg[index];
-        index++;
-        $('.displayImg').attr("src", `${displayImg[index]}`);
-        // $('.secondImg').attr("src", `${secondImg[index]}`);
-        // $('.firstImg')
-        // $('.firstImg').html(`<img class="firstImg" id="content1" src="https://www.thispersondoesnotexist.com/image" alt=""></img>`);
-        // console.log(index);
-    }
-    $('.realButton').on('click', function () {
-        changeImage(randNum);
-   } );
-    $('.fakeButton').on('click', function () {
-        changeImage(randNum);
-    })
-    
-});
+    let imgChance = Math.floor (Math.random() * 10);
 
-    let fakeImg= (randomImg());
+    let randomRealImg = Math.floor(Math.random() * 8);
 
-    if (index < 10){
-        $('displayImg') = realImg();
+    if (imgChance >= 5){
+        $('.displayImg').attr("src", `${realImg[randomRealImg]}`);
     } else {
-        $('displayImg') = fakeImg();
+        $('.displayImg').attr("src", randomFakeImg + d.getTime()); 
     }
 
-
+});
 //for MVP purposes. lets make it a 1 image on the page w/ two button  classes (choice real, choice fake)
+
 //let randNum=Math.random()
+
+// 
