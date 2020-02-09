137529b39c2e21754a72e8d8be75e19eea125c9d
LucasNemeth <l_orion_n@hotmail.com>
2020-02-09 15:48:28 -0500

    trying to figure things out

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 $(function() {
-    let index= Math.floor(Math.random() * 4);
+    let index= Math.floor(Math.random() * 20);
+
+        d=new Date();
+    
+    // let index=0;
+    let randNum=Math.random();
     let randomImg = "https://www.thispersondoesnotexist.com/image?";
-    $('.firstImg').attr("src",``);
-    $('.secondImg').attr("src", "")
+    $('.displayImg').attr("src", randomImg + d.getTime());
+    // $('.secondImg').attr("src", randomImg + d.getTime())
 
-    let firstImg = [
-        randomImg,
-        randomImg,
-        randomImg,
-        randomImg,
-        randomImg
-        // "./assets/reals/placeholder3.jpg",
-        // "./assets/reals/real1.jpg",
-        // randomImg,
-        // "../assets/reals/real2.jpg"
-    ]
-    let secondImg = [
-        // randomImg,
-        // randomImg,
-        // randomImg,
-        // randomImg,
-        // randomImg
-        "../assets/reals/placeholder1.jpg",
-        randomImg,
-        randomImg,
-        "../assets/reals/placeholder4.jpg",
-        randomImg
+
+
+    let realImg = [
+        "/assets/reals/real2.jpg",
+        "/assets/reals/placeholder1.jpg",
+        "/assets/reals/real1.jpg",
+        "/assets/reals/real3.jpeg.jpg",
+        "/assets/reals/placeholder5.jpg"
     ]
 
-    // let index;
+    
+
     function changeImage() {
         // const img = $('content1','content2').$('img')[0]
         // img.src = firstImg[index] , secondImg[index];
-        $('.firstImg').attr("src", `${firstImg[index]}`);
-        $('.secondImg').attr("src", `${secondImg[index]}`);
-        // index++;
-        console.log(index);
+        index++;
+        $('.displayImg').attr("src", `${displayImg[index]}`);
+        // $('.secondImg').attr("src", `${secondImg[index]}`);
+        // $('.firstImg')
+        // $('.firstImg').html(`<img class="firstImg" id="content1" src="https://www.thispersondoesnotexist.com/image" alt=""></img>`);
+        // console.log(index);
     }
-    $('.button1').on('click', function(){
-        changeImage();
-        console.log(changeImage)
-    })
-    $('.button2').on('click', function () {
-        changeImage();
+    $('.realButton').on('click', function () {
+        changeImage(randNum);
+   } );
+    $('.fakeButton').on('click', function () {
+        changeImage(randNum);
     })
     
 });
+
+    let fakeImg= (randomImg());
+
+    if (index < 10){
+        $('displayImg') = realImg();
+    } else {
+        $('displayImg') = fakeImg();
+    }
+
+
+//for MVP purposes. lets make it a 1 image on the page w/ two button  classes (choice real, choice fake)
+//let randNum=Math.random()
